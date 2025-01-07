05584ce0f17dcdc5e2f1900607bf9f2f16ca9f33
Evan Brooks <60624108+evanbrooks0629@users.noreply.github.com>
2025-01-07 13:33:23 -0500

    Update App.js - add MongoMail

--- src/App.js
+++ src/App.js
@@ -152,9 +152,9 @@ function App() {
               Software Engineer
             </Typography>
             <Typography variant="h6" color="secondary" sx={{ marginTop: { xs: '20px', sm: '10px', md: '10px', lg: '10px' } }} style={{fontSize: '18px', fontFamily: 'Space Mono'}}>
-              I am a software developer who specializes in creating meaningful app experiences. Currently, I'm working on my startup Brighter, a microlearning mobile application.
+              I am a software developer who specializes in creating meaningful app experiences. Currently, I'm working on my startup MongoMail, a SaaS that bridges the gap between user data and email marketing.
             </Typography>
-            <Button variant="outlined" color="success" href="https://github.com/evanbrooks0629/TaskForceAI" target="_blank" style={{marginTop: '30px', textTransform: 'none', borderWidth: '3px', fontWeight: 'bold', fontFamily: 'Space Mono'}}>View my most recent project</Button>
+            <Button variant="outlined" color="success" href="https://mongomail.co" target="_blank" style={{marginTop: '30px', textTransform: 'none', borderWidth: '3px', fontWeight: 'bold', fontFamily: 'Space Mono'}}>Check out MongoMail</Button>
           </Grid>
           <Grid item xs={2} />
           <Grid item xs={12} style={{height: '50px'}} />
@@ -168,7 +168,7 @@ function App() {
             <Grid item container xs={8} sm={6} md={6} lg={6} >
               <Typography variant="h4" color="primary" style={{fontFamily: 'Kanit'}}><span style={{color: '#0cce6b', fontFamily: 'Space Mono'}}>01.&nbsp;</span>About Me</Typography>
               <Typography variant="h6" color="secondary" style={{marginTop: "20px", fontSize: '18px', fontFamily: 'Space Mono'}}>
-                Hey there! My name is Evan Brooks, and I recently conpleted my bachelor's degree in computer science at the University of Florida. My interests lie in software development, crypto, climbing, and soccer. I love to create web apps with React and mobile apps with React Native. My most recent project is called TaskForceAI, which is a desktop application that allows for custom configuration of teams of AI agents to complete complex tasks. 
+                Hey there! My name is Evan Brooks, and I recently conpleted my bachelor's degree in computer science at the University of Florida and am currently getting my master's in software engineering from the University of Miami. My interests lie in software development, crypto, climbing, and soccer. I love to create web apps with React and mobile apps with React Native. My most recent project is called TaskForceAI, which is a desktop application that allows for custom configuration of teams of AI agents to complete complex tasks. 
               </Typography>
               <Typography variant="h6" color="secondary" style={{marginTop: "20px", fontSize: '18px', fontFamily: 'Space Mono'}}>
                 I tend to work with JavaScript, Python, and C++, but I am also knowledgable in HTML, CSS, PHP, MySQL.  Recently I have been working on projects using React and Node. 
@@ -208,6 +208,7 @@ function App() {
                 </Typography>
               </Grid>
               <Grid item container xs={12} style={{marginTop: '30px', position: 'relative'}}>
+                <Project title="MongoMail" link1="" link2="https://mongomail.co" subtitle="MongoMail connects to various databases, like MongoDB, and email providers, like SendGrid, to seamlessly bridge the gap between user data and email marketing. Key features include user segmentation, email template management, valuable deliverability analytics, and complete email personalization. Built with Node.js, Express.js, Next.js, AWS EC2, Ngrok, PM2, Stripe, & Unlayer. Currently supports integrations for MongoDB, Gmail, and SendGrid." img1={taskforce} />
                 <Project title="TaskForceAI" link1="https://github.com/evanbrooks0629/TaskForceAI" link2="https://youtu.be/npXj7mpZvDQ" subtitle="TaskForceAI is a desktop application that allows for custom configuration of teams of AI agents to complete complex tasks. Users can choose from multiple LLMs to run tasks. By configuring teams of agents, each task can be worked on by multiple agents simultaneously. Built using PyQt6 and Microsoft Autogen in Python." img1={taskforce} />
                 <Project title="XRPH Wallet" link1="https://apps.apple.com/us/app/xrph-wallet/id6451218628" link2="https://github.com/XRPHealthcare/XRPH-Mobile-Wallet" subtitle="A non-custodial mobile wallet for the XRP Ledger. Users can send and receive XRP and XRPH, track transaction history, and get rewards using the XRPH Prescription Savings Card. Built with React Native, Firebase, Google Cloud, and XRPL.js. Visit the link to find out how to download it and the Github repository to view the open source code." img1={xrph} />
                 <Project title="TimeBlock" link1="https://github.com/evanbrooks0629/TimeBlock" link2="https://evanbrooks0629.github.io/TimeBlock/" subtitle="Inspired by the popular TimeBlocking technique, I set out to create an app that let me do that, but using technology. This PWA lets you visually plan out your day by using things called 'blocks,' which can be any event. It is quite useful when you know you have things to do, but are having trouble planning out fitting everything into your day." img1={timeblock} />
